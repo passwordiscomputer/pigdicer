123178dd319f6ef3efea4cff6e9dde5a71ec8dcb
Chase Bowden and Johnny Mayers <johnny@>
2018-01-24 14:13:23 -0800

    Back end logic updates: roll, turn functions complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-function playerFirst(min, max) {
-    Math.floor(Math.random() * 2) + 1;
-}
-
+//BACK END
 function roll() {
   var num = Math.floor(Math.random() * 6) + 1;
   if ( num === 1) {
@@ -10,3 +7,32 @@ function roll() {
     return num;
   }
 }
+
+function turn() {
+  var choice = true;
+  var points = 0;
+  while(choice === true) {
+    choice = confirm("Would you like to roll again?"); /*REMOVE CONFIRM BOX WHEN INTEGRATING PLAYER FUNCTIONS*/
+    var num = roll();
+    if (num === 0) {
+    points = 0;
+    return points;
+  } if (choice === false) {
+      points = (points + num);
+      return points;
+    } else if (choice === true) {
+      points = (points + num);
+    }
+  }
+}
+
+function play() {
+  var pointTotal = 0;
+  if points = 100; {
+    /*DISPLAY WIN MESSAGE*/
+  } else {
+    /*DISPLAY WIN CONDITION NOT MET*/
+  }
+}
+
+//FRONT END

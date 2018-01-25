1836db40a9fe1d61804e53cb491035c44f2b9be0
Guest User <Guest@Epicodus-7D.local>
2018-01-25 11:16:46 -0800

    Final functionality complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 //BACKY
 function Player(name) {
   this.name = name;
-  this.points = 0;
-}
-
-function Game() {
-  this.playerArray = [];
-
+  this.totalScore = 0;
+  this.roundScore = 0;
 }
 
 function roll() {
@@ -18,49 +14,78 @@ function roll() {
   }
 }
 
-Player.prototype.turn = function() {
-  var choice = true;
-  var points = 0;
-  while(choice === true) {
-    var num = roll();
-    $("#currentRoll").text(num);
-    if (num === 0) {
-    points = 0;
-    return points;
-  } if (choice === false) {
-      points = (points + num);
-      this.points = this.points + points;
-      return points;
-    } else if (choice === true) {
-      this.points = (points + num);
-    }
-  }
-}
-
-function playerStart() {
-  var num = Math.floor(Math.random() * 2) + 1;
-  if (num === 1) {
-    return alert("Player X starts.");
-  } else {
-    return alert("Player Y starts.");
-  }
-}
-
-// function play() {
-//   while (100 > player.points) {
-//   }
-// }
-//FRONTY
-
 $(document).ready(function() {
   $("form#playerNameInput").submit(function(event) {
-    var game1 = new Game();
-    game1.playerArray.push(new Player("Chase"));
-    game1.playerArray.push(new Player("Johnny"));
-    game1.playerArray[1].turn();
+    var player1 = new Player($("#player1").val());
+    var player2 = new Player($("#player2").val());
+    $("#player1Display").text(player1.name);
+    $("#player2Display").text(player2.name);
+    $("#currentRoll").text(player1.name + " please hit roll");
     event.preventDefault();
-  });
-});
+    $("#roll1").last().click(function(){
+      var currentRoll = roll();
+      $("#currentRoll").text(currentRoll);
+      if (currentRoll === 0){
+        console.log("You lost Son");
+        player1.roundScore = 0;
+        $("#bank1").text(player1.roundScore);
+        $("#roll1").toggle();
+        $("#roll2").toggle();
+        $("#hold1").toggle();
+        $("#hold2").toggle();
+        $("#currentRoll").text(player1.name + " lost all points this round!");
+      } else {
+        player1.roundScore += currentRoll;
+        $("#bank1").text(player1.roundScore);
+      }
 
-$("#roll").click(function() {
+      });
+      $("#roll2").last().click(function(){
+        var currentRoll = roll();
+        $("#currentRoll").text(currentRoll);
+        if (currentRoll === 0){
+          console.log("You lost Son");
+          player2.roundScore = 0;
+          $("#bank2").text(player2.roundScore);
+          $("#roll1").toggle();
+          $("#roll2").toggle();
+          $("#hold1").toggle();
+          $("#hold2").toggle();
+          $("#currentRoll").text(player2.name + " lost all points this round!");
+        } else {
+          player2.roundScore += currentRoll;
+          $("#bank2").text(player2.roundScore);
+        }
+        });
+    $("#hold1").last().click(function(){
+      player1.totalScore += player1.roundScore;
+      $("#total1").text(player1.totalScore);
+      $("#currentRoll").text(player1.name + " scored " + player2.roundScore + " points!");
+      player1.roundScore = 0;
+      $("#bank1").text(player1.roundScore);
+      if (player1.totalScore >= 50){
+        alert(player1.name + " has won because " + player1.name + " is better than " + player2.name + ".");
+        location.reload();
+      };
+      $("#roll1").toggle();
+      $("#roll2").toggle();
+      $("#hold1").toggle();
+      $("#hold2").toggle();
+    });
+      $("#hold2").last().click(function(){
+        player2.totalScore += player2.roundScore;
+        $("#total2").text(player2.totalScore);
+        $("#currentRoll").text(player2.name + " scored " + player2.roundScore + " points!");
+        player2.roundScore = 0;
+        $("#bank2").text(player2.roundScore);
+        if (player2.totalScore >= 50){
+          alert(player2.name + " has won because " + player2.name + " is better than " + player1.name + ".");
+          location.reload();
+        };
+        $("#roll1").toggle();
+        $("#roll2").toggle();
+        $("#hold1").toggle();
+        $("#hold2").toggle();
+      });
+  });
 });
